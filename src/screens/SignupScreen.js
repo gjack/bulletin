10180f150f38e51b0c0d628dbfe511332082fb26
gjack <gabi@ministrycentered.com>
2020-05-04 15:23:16 -0400

    Use react-native-elements to wire up sign up screen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,18 +1,39 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import { FontAwesome } from "@expo/vector-icons";
 
 const SignupScreen = ({ navigation }) => {
   return (
     <>
-      <Text style={{ fontSize: 40 }}>SignupScreen</Text>
-      <Button
-        title="Go to Sign In"
-        onPress={() => navigation.navigate("Signin")}
+      <Text h3>Sign Up for Bulletin</Text>
+      <Input label="First Name" />
+      <Input label="Last Name" />
+      <Input
+        label="Email"
+        placeholder="email@address.com"
+        leftIcon={
+          <FontAwesome
+            name="envelope"
+            style={{ fontSize: 25, marginRight: 10 }}
+          />
+        }
       />
-      <Button
-        title="Go to mainFlow"
-        onPress={() => navigation.navigate("mainFlow")}
+      <Input
+        label="Password"
+        placeholder="your password"
+        leftIcon={
+          <FontAwesome name="lock" style={{ fontSize: 25, marginRight: 10 }} />
+        }
       />
+      <Input
+        label="Password Confirmation"
+        placeholder="confirm your password"
+        leftIcon={
+          <FontAwesome name="lock" style={{ fontSize: 25, marginRight: 10 }} />
+        }
+      />
+      <Button title="Sign Up" />
     </>
   );
 };
