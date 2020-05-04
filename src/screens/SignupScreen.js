de03a1e44b3e4335b71cf42f41923668f70f7835
gjack <gabi@ministrycentered.com>
2020-05-04 15:43:14 -0400

    Tweak screen styles

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -5,39 +5,46 @@ import { FontAwesome } from "@expo/vector-icons";
 
 const SignupScreen = ({ navigation }) => {
   return (
-    <>
+    <View style={styles.containerStyles}>
       <Text h3>Sign Up for Bulletin</Text>
       <Input label="First Name" />
       <Input label="Last Name" />
       <Input
-        label="Email"
+        label="Your email address"
         placeholder="email@address.com"
-        leftIcon={
-          <FontAwesome
-            name="envelope"
-            style={{ fontSize: 25, marginRight: 10 }}
-          />
-        }
+        leftIcon={<FontAwesome name="envelope" style={styles.iconStyles} />}
       />
       <Input
         label="Password"
-        placeholder="your password"
-        leftIcon={
-          <FontAwesome name="lock" style={{ fontSize: 25, marginRight: 10 }} />
-        }
+        placeholder="Password"
+        leftIcon={<FontAwesome name="lock" style={styles.iconStyles} />}
       />
       <Input
         label="Password Confirmation"
         placeholder="confirm your password"
-        leftIcon={
-          <FontAwesome name="lock" style={{ fontSize: 25, marginRight: 10 }} />
-        }
+        leftIcon={<FontAwesome name="lock" style={styles.iconStyles} />}
       />
       <Button title="Sign Up" />
-    </>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  iconStyles: {
+    fontSize: 25,
+    marginRight: 10,
+  },
+  containerStyles: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 200,
+  },
+});
 
 export default SignupScreen;
