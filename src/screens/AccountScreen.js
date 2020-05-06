9a291ad12f7fa03a5896c5416aca5e79db019940
gjack <gabi@ministrycentered.com>
2020-05-06 10:57:32 -0400

    Wire up Account screen

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,8 +1,17 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
+import { Text, Button } from "react-native-elements";
+import { SafeAreaView } from "react-navigation";
+import { Context as AuthContext } from "../context/AuthContext";
 
 const AccountScreen = () => {
-  return <Text style={{ fontSize: 40 }}>AccountScreen</Text>;
+  const { signOut } = useContext(AuthContext);
+  return (
+    <SafeAreaView>
+      <Text h3>Account</Text>
+      <Button title="Sign Out" onPress={signOut} />
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({});
